07067d2e42870f14f96c989ba3064ab8f3bfeac7
tushima <148033059+tushiko23@users.noreply.github.com>
2026-01-12 19:39:32 +0900

    トップ画面の作成しrootに設定と新規作成とログインでrootに遷移するようにする

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,17 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
+  protected
+
   def configure_permitted_parameters
   devise_parameter_sanitizer.permit(:sign_up, keys: [ :name ])
+  end
+
+  def after_sign_up_for_path(resource)
+    root_path
+  end
+
+  def after_sign_in_for_path(resource)
+    root_path
   end
 end
