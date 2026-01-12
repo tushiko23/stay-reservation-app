0677ea569d11801d6e1834eca8350e41919ce7b0
tushima <148033059+tushiko23@users.noreply.github.com>
2026-01-13 05:26:25 +0900

    ユーザモデルにユーザ名の入力を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  validates :name, presence: { message: "を入力してください" }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
