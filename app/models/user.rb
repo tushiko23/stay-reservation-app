d004f7a6e66e2fd1fa0a9c99a855b89ac718e006
tushima <148033059+tushiko23@users.noreply.github.com>
2026-01-18 14:22:41 +0900

    UserモデルとProfileモデルの関連付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_one :profile, dependent: :destroy
   validates :name, presence: { message: "を入力してください" }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
