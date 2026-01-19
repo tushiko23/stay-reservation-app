b12bd691bbb5356ea49f8be185edb758b48ea50b
tushima <148033059+tushiko23@users.noreply.github.com>
2026-01-20 03:26:33 +0900

    user_attributesをキーとして使うためモデルに設定とコントローラに編集更新アクションを追加

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,45 @@
+class ProfilesController < ApplicationController
+  before_action :authenticate_user!
+  def edit
+    @profile = current_user.profile || current_user.build_profile
+    @profile.user = current_user
+  end
+
+  def update
+    @profile = current_user.profile || current_user.build_profile
+    @user = current_user
+
+    pp = profile_params
+
+    name = pp.dig(:user_attributes, :name).to_s.strip
+    intro = pp[:introduction].to_s.strip
+    icon_present = pp.key?(:icon)
+
+    if name.empty? && intro.empty? && !icon_present
+      flash.now[:alert] = "どれか1つは入力してください"
+      return render :edit, status: :unprocessable_entity
+    end
+
+  ActiveRecord::Base.transaction do
+    profile_updates = {}
+    profile_updates[:introduction] = intro unless intro.empty?
+    profile_updates[:icon] = pp[:icon] if icon_present
+
+    @profile.update!(profile_updates) if profile_updates.any?
+    @user.update!(name: name) unless name.empty?
+  end
+
+  redirect_to settings_path, notice: 'プロフィールが更新されました'
+
+  rescue ActiveRecord::RecordInvalid
+    @user = current_user
+    flash.now[:alert] = "プロフィールの更新に失敗しました"
+    render :edit, status: :unprocessable_entity
+  end
+
+  private
+
+    def profile_params
+      params.require(:profile).permit(:introduction, :icon, user_attributes: [ :id, :name ])
+    end
+end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,6 @@
 class Profile < ApplicationRecord
   belongs_to :user
   has_one_attached :icon
+
+  accepts_nested_attributes_for :user
 end
