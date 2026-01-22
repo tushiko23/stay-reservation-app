5a08edeba3a2ea128f9f4e82636c1ec4237ba45f
tushima <148033059+tushiko23@users.noreply.github.com>
2026-01-22 19:02:08 +0900

    rooms/ownにユーザが作成した部屋一覧を表示、roomsにルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: "home#index"
-
+  get "rooms/own", to: "rooms#own"
+  resources :rooms
   scope :users do
     resource :settings, only: [ :show ]
     post "settings/profile", to: "profiles#update"
@@ -11,6 +12,7 @@ Rails.application.routes.draw do
       resource :profile, only: [ :edit ]
     end
   end
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
