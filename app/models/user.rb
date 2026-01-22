ae032cf0f2a171ea0c22a5e3c896136cb0b60c9c
tushima <148033059+tushiko23@users.noreply.github.com>
2026-01-22 19:00:18 +0900

    UserモデルにRoomモデルを1対多でリレーションする

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_one :profile, dependent: :destroy
+  has_many :rooms, dependent: :destroy
   validates :name, presence: { message: "を入力してください" }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
