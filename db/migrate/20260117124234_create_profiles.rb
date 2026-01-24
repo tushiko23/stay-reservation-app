d7522443e459dff2fb5f7ec8cff2380d46a7479a
tushima <148033059+tushiko23@users.noreply.github.com>
2026-01-24 19:06:46 +0900

    user_idの関連付けのバリデーションnull: falseに修正

--- db/migrate/20260117124234_create_profiles.rb
+++ db/migrate/20260117124234_create_profiles.rb
@@ -1,7 +1,7 @@
 class CreateProfiles < ActiveRecord::Migration[7.2]
   def change
     create_table :profiles do |t|
-      t.references :user, null: :false, foreign_key: true, index: { unique: true }
+      t.references :user, null: false, foreign_key: true, index: { unique: true }
       t.text :introduction
       t.timestamps
     end
