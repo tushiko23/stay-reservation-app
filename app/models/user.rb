f194bb87613d6f532f56014578d795419cba29a1
tushima <148033059+tushiko23@users.noreply.github.com>
2026-01-24 19:13:17 +0900

    roomモデルとuserモデルにreservationモデルの中間モデルの関連付けを定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_one :profile, dependent: :destroy
   has_many :rooms, dependent: :destroy
+  has_many :reservations, dependent: :destroy
+  has_many :reserved_rooms, through: :reservations, source: :room
   validates :name, presence: { message: "を入力してください" }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
