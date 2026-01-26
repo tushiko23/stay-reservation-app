577e0c03269a77455fcfe7ef27cf7c0573500faa
tushima <148033059+tushiko23@users.noreply.github.com>
2026-01-26 17:51:52 +0900

    不要なアクティブレコードの関連付けを削除

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,7 +2,6 @@ class Room < ApplicationRecord
   belongs_to :user
 
   has_many :reservations, dependent: :destroy
-  has_many :reserved_users, through: :reservations, source: :user
   has_one_attached :image
 
   validates :name, presence: true
