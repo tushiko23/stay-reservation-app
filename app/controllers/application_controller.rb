6b974004af528a8945d2f788096ba82929cec8a8
tushima <148033059+tushiko23@users.noreply.github.com>
2026-01-26 18:02:30 +0900

    ログイン時にトップではなく作成しようとしていたページに移動するよう設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_header_profile
+  before_action :store_user_location!, if: :storable_location?
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
@@ -15,13 +16,24 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    root_path
+    stored_location_for(resource) || root_path
   end
 
   private
 
-    def set_header_profile
-      return unless user_signed_in?
-      @profile = current_user.profile || current_user.build_profile
-    end
+  def set_header_profile
+    return unless user_signed_in?
+    @profile = current_user.profile || current_user.build_profile
+  end
+
+  def storable_location?
+    request.get? &&
+      is_navigational_format? &&
+      !devise_controller? &&
+      !request.xhr?
+  end
+
+  def store_user_location!
+    store_location_for(:user, request.fullpath)
+  end
 end
