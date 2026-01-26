8f2b6010cb78ce030d68bb639856fd1f37901d18
tushima <148033059+tushiko23@users.noreply.github.com>
2026-01-26 17:55:41 +0900

    予約作成の新規フォームを作成

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,31 +1,62 @@
 class ReservationsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_reservation, only: [ :show, :edit, :update, :destroy ]
+  before_action :set_room_reservation, only: [ :new, :confirm, :create ]
   def index
     @reservations = current_user.reservations
   end
 
   def new
-    
+    @reservation = current_user.reservations.build(room: @room)
+  end
+
+  def confirm
+    @reservation = current_user.reservations.build(reservation_params.merge(room: @room))
   end
 
   def create
-    
+    @reservation = current_user.reservations.build(reservation_params.merge(room: @room))
+
+    if @reservation.save
+      redirect_to reservations_path, notice: "予約が作成されました"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
+
 
   def show
     
   end
 
   def edit
-    
+
   end
 
   def update
-    
+    if @reservation.update(reservation_params)
+      redirect_to reservation_path(@reservation), notice: "施設先を編集しました。"
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
-    
+    @reservation.destroy
+    redirect_to reservations_path, status: :see_other, notice: "予約を削除しました"
   end
 
+  private
+
+  def set_reservation
+    @reservation = current_user.reservations.find(params[:id])
+  end
+
+  def set_room_reservation
+    @room = Room.find(params[:room_id])
+  end
+
+  def reservation_params
+    params.require(:reservation).permit(:check_in_date, :check_out_date, :guest_count, :room_id)
+  end
 end
