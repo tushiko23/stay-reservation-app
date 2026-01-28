00f98fdc915473e4c10ca91fbedc95b5a8478f47
tushima <148033059+tushiko23@users.noreply.github.com>
2026-01-28 15:51:46 +0900

    画像をアップロードするときにファイル名を表示

--- app/javascript/show_profile_icon_filename.js
+++ app/javascript/show_profile_icon_filename.js
@@ -1,17 +1,16 @@
   // プロフィールのアイコンアップロード時にファイル名を表示
 
-  document.addEventListener('DOMContentLoaded', () => {
-    const input = document.getElementById('profile_icon');
-    const fileName = document.querySelector('.file-name');
+document.addEventListener('turbo:load', () => {
+  const input = document.getElementById('profile_icon');
+  const fileName = document.querySelector('.file-name');
 
-    if (!input || !fileName) return;
+  if (!input || !fileName) return;
 
-    input.addEventListener('change', () => {
-      if (input.files.length > 0) {
-        fileName.textContent = input.files[0].name;
-      } else {
-        fileName.textContent = '選択されていません';
-      }
-    });
+  input.addEventListener('change', () => {
+    if (input.files.length > 0) {
+      fileName.textContent = input.files[0].name;
+    } else {
+      fileName.textContent = '選択されていません';
+    }
   });
-
+});
