c44fdc4e1bb151b71477a70bd3ea6f9310ac1750
tushima <148033059+tushiko23@users.noreply.github.com>
2026-01-28 15:49:37 +0900

    ユーザ名が空白なら入力できないようにする

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :rooms, dependent: :destroy
   has_many :reservations, dependent: :destroy
   validates :name, presence: { message: "を入力してください" }
+  validates :name, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
