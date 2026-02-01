df5b94444b3b9be6cbd7657b43e3906dc8a5eb12
tushima <148033059+tushiko23@users.noreply.github.com>
2026-02-01 18:15:30 +0900

    ファイルアップロード時にファイル名を表示するjsを実装

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,4 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import "./show_profile_icon_filename"
+import "./img_upload_show_filename"
